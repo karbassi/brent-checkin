902e1ea5d2a78c3ac0379422967bf40edc1cd07d
svcastaneda <svcastaneda@gmail.com>
2018-02-22 17:05:23 -0600

    Add in LoginViewController stuffs (found in a tutorial)

--- Check In/Controllers/LoginViewController.swift
+++ Check In/Controllers/LoginViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    let loginToList = "LoginToList"
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func loginDidTouch(_ sender: Any) {
+        performSegue(withIdentifier: loginToList, sender: nil)
+    }
+    
+    @IBAction func signUpDidTouch(_ sender: Any) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { action in
+                                        
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,4 +68,18 @@ class LoginViewController: UIViewController {
     }
     */
 
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField {
+            passwordField.becomeFirstResponder()
+        }
+        if textField == passwordField {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
 }
