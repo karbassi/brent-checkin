be7ac14e37ad4ee2b5713dd0d343d2fe8aaf28a6
svcastaneda <svcastaneda@gmail.com>
2018-02-22 02:53:19 -0600

    New view controller for adding new reports

---
+++ Check In/Controllers/NewReportViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NewReportViewController.swift
+//  Check In
+//
+//  Created by Saundra Castaneda on 2/22/18.
+//  Copyright © 2018 DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class NewReportViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let fixed = roundf(sender.value);
+        sender.setValue(fixed, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
