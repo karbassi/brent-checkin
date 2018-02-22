6861df4f6227a2f36f88a49f97d3dee5dd3dd3a7
svcastaneda <svcastaneda@gmail.com>
2018-02-22 02:51:47 -0600

    Initial default view controller

---
+++ Check In/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check In
+//
+//  Created by Saundra Castaneda on 2/21/18.
+//  Copyright © 2018 DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
