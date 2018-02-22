0c3a1282330649fc209a539ed7f47beb37c8bd32
svcastaneda <svcastaneda@gmail.com>
2018-02-22 17:06:38 -0600

    Create User model

---
+++ Check In/Models/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  Check In
+//
+//  Created by Saundra Castaneda on 2/22/18.
+//  Copyright © 2018 DePaul University. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct User {
+    
+    let uid: String
+    let email: String
+    
+    init(authData: User) {
+        uid = authData.uid
+        email = authData.email
+    }
+    
+    init(uid: String, email: String) {
+        self.uid = uid
+        self.email = email
+    }
+    
+}
