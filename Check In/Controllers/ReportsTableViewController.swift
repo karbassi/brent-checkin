62d671a9f0215e1c23baab2611c0cc56e20e8365
svcastaneda <svcastaneda@gmail.com>
2018-02-25 01:52:50 -0600

    Prepare cell for details segue

--- Check In/Controllers/ReportsTableViewController.swift
+++ Check In/Controllers/ReportsTableViewController.swift
@@ -121,14 +121,17 @@ class ReportsTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if let detailViewController = segue.destination as? DetailReportViewController {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                detailViewController.report = reportList[indexPath.row]
+            }
+        }
     }
-    */
 
 }
