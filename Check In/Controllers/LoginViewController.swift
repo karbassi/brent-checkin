709daedc74d532803a7939da917294be094e4a8a
svcastaneda <svcastaneda@gmail.com>
2018-02-25 22:09:39 -0600

    Make everything move up when keyboard appears

--- Check In/Controllers/LoginViewController.swift
+++ Check In/Controllers/LoginViewController.swift
@@ -68,6 +68,8 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
 
         // Do any additional setup after loading the view.
         Auth.auth().addStateDidChangeListener() { auth, user in
@@ -110,6 +112,21 @@ class LoginViewController: UIViewController {
         return reportList.reversed()
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
 }
 
 extension LoginViewController: UITextFieldDelegate {
